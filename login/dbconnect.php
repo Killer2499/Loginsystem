d0e0d02e11cebdb8670c2e57d224ad2aa5c9d344
Sanath <30856736+Killer2499@users.noreply.github.com>
2018-03-06 00:00:42 +0530

    Add files via upload

---
+++ login/dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+
+$dbc=mysqli_connect('localhost','root','','login')
+     or die("Unable to connect to database");//Conneting to  database
+     //This is in the form of ('hostname','username','password','database name')
+     //The details are available in phpmyadmin-->user accounts
+?>
